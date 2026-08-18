9ccff51a9faf7f7b33e8819a8c80c5c6201b684d
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-18 12:46:23 +0300

    remove admin login password length restriction

--- frontend/admin/index.jsx
+++ frontend/admin/index.jsx
@@ -2,5 +2,14 @@
 import * as ReactDOM from 'react-dom/client';
 import App from './app';
 
+const removeLoginPasswordMinLength = () => {
+  const passwordInput = document.querySelector('.auth-form input[type="password"]');
+  if (passwordInput) passwordInput.removeAttribute('minlength');
+};
+
+const observer = new MutationObserver(removeLoginPasswordMinLength);
+observer.observe(document.documentElement, { childList: true, subtree: true });
+
 const root = ReactDOM.createRoot(document.querySelector('#admin-root'));
 root.render(<App />);
+removeLoginPasswordMinLength();
