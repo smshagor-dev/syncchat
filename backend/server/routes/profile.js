bed092759ec255b1cade84996d0101d7b1db5a21
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-18 18:45:08 +0300

    expose profile photo history routes

--- backend/server/routes/profile.js
+++ backend/server/routes/profile.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 const authenticate = require('../middleware/auth');
 const ctrl = require('../controllers/profile');
 
-router.get('/profiles/:userId', authenticate, ctrl.findById);
+router.get('/profile-photos', authenticate, ctrl.resolvePhotoHistory);
+router.delete('/profile-photos/:photoId', authenticate, ctrl.deleteProfilePhoto);
+router.get('/profiles/:userId/photos', authenticate, ctrl.profilePhotos);
 router.get('/profiles/:userId/common-groups', authenticate, ctrl.commonGroups);
+router.get('/profiles/:userId', authenticate, ctrl.findById);
 router.put('/profiles', authenticate, ctrl.edit);
 
 module.exports = router;
