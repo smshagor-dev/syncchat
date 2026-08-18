7066a14d504cc006bdb6581a4dad3c1b4cf138be
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-18 14:04:44 +0300

    use websocket-only Socket.IO transport

--- frontend/client/helpers/socket.js
+++ frontend/client/helpers/socket.js
@@ -2,8 +2,12 @@ import { io } from 'socket.io-client';
 import config from '../config';
 
 const socket = io(config.socketUrl, {
+  path: '/socket.io',
+  transports: ['websocket'],
   autoConnect: false,
-  reconnectionAttempts: 5,
-  reconnectionDelay: 1500,
+  reconnection: true,
+  reconnectionAttempts: 10,
+  reconnectionDelay: 1000,
+  reconnectionDelayMax: 5000,
 });
 export default socket;
