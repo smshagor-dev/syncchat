6cb88a35b802c886518bcd0d37313e621f21714b
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-18 13:42:55 +0300

    register PWA from admin

--- frontend/admin/index.jsx
+++ frontend/admin/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './app';
+import { registerServiceWorker } from '../client/pwa/registerSW';
 import { installProfilePasswordPanel } from './profilePassword';
 
 const removeLoginPasswordMinLength = () => {
@@ -15,3 +16,4 @@ const root = ReactDOM.createRoot(document.querySelector('#admin-root'));
 root.render(<App />);
 removeLoginPasswordMinLength();
 installProfilePasswordPanel();
+registerServiceWorker();
