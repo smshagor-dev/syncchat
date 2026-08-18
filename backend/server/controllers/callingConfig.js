7cd0bc6cc4d808c264d83f13c16baaae2638e08c
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-18 17:38:18 +0300

    add authenticated SFU join token endpoint

--- backend/server/controllers/callingConfig.js
+++ backend/server/controllers/callingConfig.js
@@ -1,5 +1,6 @@
 const response = require('../helpers/response');
 const { getCallRuntimeConfig } = require('../helpers/callConfig');
+const { getLiveKitJoinCredentials } = require('../helpers/livekit');
 
 exports.getRuntimeConfig = async (req, res) => {
   try {
@@ -11,6 +12,24 @@ exports.getRuntimeConfig = async (req, res) => {
       statusCode: 500,
       success: false,
       message: error0.message || 'Unable to load calling configuration',
+    });
+  }
+};
+
+exports.getSfuToken = async (req, res) => {
+  try {
+    const payload = await getLiveKitJoinCredentials({
+      callId: req.body?.callId,
+      userId: req.user?._id,
+      displayName: req.user?.fullname || req.user?.username || '',
+    });
+    response({ res, payload });
+  } catch (error0) {
+    response({
+      res,
+      statusCode: error0.statusCode || 500,
+      success: false,
+      message: error0.message || 'Unable to authorize group media session',
     });
   }
 };
