e553515a43fb3c97cbf992df7b928934dd40c6d4
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-18 17:47:02 +0300

    route call media mode lookup

--- backend/server/routes/callingConfig.js
+++ backend/server/routes/callingConfig.js
@@ -3,6 +3,7 @@ const authenticate = require('../middleware/auth');
 const callingConfig = require('../controllers/callingConfig');
 
 router.get('/calling/config', authenticate, callingConfig.getRuntimeConfig);
+router.get('/calling/session/:callId', authenticate, callingConfig.getSessionMedia);
 router.post('/calling/sfu-token', authenticate, callingConfig.getSfuToken);
 
 module.exports = router;
