fbfa968243ad0e98ca80c51995f250c32e0f057b
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-19 23:46:47 +0300

    feat: expose app-config PWA icon endpoints

--- backend/server/routes/storagePublic.js
+++ backend/server/routes/storagePublic.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const controller = require('../controllers/storagePublic');
 
 router.get('/storage/image', controller.image);
+router.get('/pwa/icon/:size.png', controller.pwaIcon);
 
 module.exports = router;
