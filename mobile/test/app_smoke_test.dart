9cfd075ee7908c103b5dac87c19e908c950ed93d
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-25 23:53:07 +0300

    test(flutter): inject in-memory session into app smoke test

--- mobile/test/app_smoke_test.dart
+++ mobile/test/app_smoke_test.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:syncchat_mobile/core/app_services.dart';
+import 'package:syncchat_mobile/core/session_store.dart';
 import 'package:syncchat_mobile/main.dart';
 import 'package:syncchat_mobile/screens.dart';
 import 'package:syncchat_mobile/theme.dart';
 
 void main() {
   testWidgets('SyncChat mobile app renders auth shell', (tester) async {
-    await tester.pumpWidget(const SyncChatMobileApp());
-    await tester.pump();
+    final services = AppServices.create(sessionStore: MemorySessionStore());
+    await tester.pumpWidget(SyncChatMobileApp(services: services));
+    await tester.pumpAndSettle();
 
     expect(find.text('SyncChat'), findsOneWidget);
     expect(find.text('Sign in'), findsWidgets);
