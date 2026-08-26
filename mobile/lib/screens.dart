59aaac9dc05d21979d5509ba86708bb113f2d002
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-26 09:54:17 +0300

    mobile(auth): activate final full-screen auth screen

--- mobile/lib/screens.dart
+++ mobile/lib/screens.dart
@@ -1,4 +1,4 @@
-export 'screens/auth_screen.dart';
+export 'screens/final_auth_screen.dart';
 export 'screens/chat_room_screen.dart';
 export 'screens/core_screens.dart';
 export 'screens/mobile_shell.dart';
