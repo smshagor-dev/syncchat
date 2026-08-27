c02e16887b78366d395f630d5d147daa7074f320
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-28 01:24:28 +0300

    fix(desktop): configure static VC runtime in build script

--- desktop/src-tauri/build.rs
+++ desktop/src-tauri/build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    tauri_build::build();
+    let windows = tauri_build::WindowsAttributes::new().static_vc_runtime(true);
+    let attributes = tauri_build::Attributes::new().windows_attributes(windows);
+
+    tauri_build::try_build(attributes).expect("failed to build SyncChat desktop resources");
 }
