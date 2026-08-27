92252e76a2da4dfb7f32e90bb792d86bc7e15576
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-28 01:37:49 +0300

    fix(desktop): use released Tauri build API

--- desktop/src-tauri/build.rs
+++ desktop/src-tauri/build.rs
@@ -1,6 +1,3 @@
 fn main() {
-    let windows = tauri_build::WindowsAttributes::new().static_vc_runtime(true);
-    let attributes = tauri_build::Attributes::new().windows_attributes(windows);
-
-    tauri_build::try_build(attributes).expect("failed to build SyncChat desktop resources");
+    tauri_build::build();
 }
