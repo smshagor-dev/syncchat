aeeb3a0ea4b9aa39525f2b0f0fbab5b754b9fb9d
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-28 10:00:08 +0300

    style(web): load desktop parity guard

--- frontend/client/index.jsx
+++ frontend/client/index.jsx
@@ -24,6 +24,7 @@ import './styles/callScreenTheme.css';
 import './styles/inboxMenuMobile.css';
 import './styles/desktopMessenger.css';
 import './styles/desktopPages.css';
+import './styles/webDesktopParity.css';
 
 import { registerServiceWorker } from './pwa/registerSW';
 
