6759110389cc676ac6d60157ab63ae59ead16504
Shahanur Islam Shagor <smshagor.ru@gmail.com>
2026-08-30 21:58:34 +0300

    fix(mobile): wrap automatic phone contacts screen safely

--- mobile/lib/screens/live_p0_contacts_screen.dart
+++ mobile/lib/screens/live_p0_contacts_screen.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
+
 import 'live_device_contacts_screen.dart';
 
-class LiveP0ContactsScreen extends LiveDeviceContactsScreen {
+class LiveP0ContactsScreen extends StatelessWidget {
   const LiveP0ContactsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const LiveDeviceContactsScreen();
 }
